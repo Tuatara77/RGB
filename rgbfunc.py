4a980dcc26420a8f9d5aca3af530e220f7dda7ec
Tuatara_77 <44623766+Tuatara77@users.noreply.github.com>
2022-04-22 09:49:59 +0100

    corrected str to int

--- rgbfunc.py
+++ rgbfunc.py
@@ -1,5 +1,5 @@
 def linear_rgb(rgb: tuple, changerate: int=3, minbrightness: int=0, maxbrightness: int=255) -> tuple:
-    assert len(rgb) == 3 and type(rgb[0]) == type(rgb[1]) == type(rgb[2]) == str, "Tuple must be 3 integers"
+    assert len(rgb) == 3 and type(rgb[0]) == type(rgb[1]) == type(rgb[2]) == int, "Tuple must be 3 integers"
     assert rgb[0] == maxbrightness or rgb[1] == maxbrightness or rgb[2] == maxbrightness and \
            rgb[0] == minbrightness or rgb[1] == minbrightness or rgb[2] == minbrightness, "There must be values which are equal to the min and max brightness"
     r, g, b = rgb
